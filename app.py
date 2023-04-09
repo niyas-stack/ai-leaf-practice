075888e775890c0d3a0511a84522de1a9cf8831f
niyas-stack <126703484+niyas-stack@users.noreply.github.com>
2023-04-09 13:43:37 +0530

    Update app.py

--- app.py
+++ app.py
@@ -66,7 +66,7 @@ def main():
     uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"])
     if uploaded_file is not None:
         image = Image.open(uploaded_file)
-        st.image(image, caption='Uploaded Image', use_column_width=True)
+        st.image(image, caption='Uploaded Image', width=500)
         st.write("")
         st.write("Classifying...")
         pred, probs = model_predict(image, model, transform)
