5c024565c222688fdf02afd571d5c5aab223ca11
niyas-stack <126703484+niyas-stack@users.noreply.github.com>
2023-05-07 21:03:16 +0530

    Update app.py

--- app.py
+++ app.py
@@ -66,7 +66,7 @@ def model_predict(image, model_func, transform):
 def display_remedies(pred):
     remedy = remedies.get(pred)
     if remedy:
-        st.info(f"Remedy: {remedy}")
+        st.write(f"Remedy: {remedy}")
         st.info("Please note that the remedies suggested are for educational purposes only. Consult a professional before using them.")
 
 def main():
