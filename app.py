5e6d827d0dae88e438e8e8da8f2121a41cf3371f
niyas-stack <126703484+niyas-stack@users.noreply.github.com>
2023-05-08 21:40:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -69,23 +69,8 @@ def display_remedies(pred):
         st.write("remedy:")
         st.info(f" {remedy}")
       
-def add_bg_from_local(image_file):
-    with open(image_file, "rb") as image_file:
-        encoded_string = base64.b64encode(image_file.read())
-    st.markdown(
-    f"""
-    <style>
-    .stApp {{
-        background-image: url(data:image/{"jpg"};base64,{encoded_string.decode()});
-        background-size: cover
-    }}
-    </style>
-    """,
-    unsafe_allow_html=True
-    )
 
 def main():
-   add_bg_from_local('background.jpg')  
    st.set_page_config(page_title="AI Leaf Disease Detection", page_icon=":leaves:")
    st.title("AI Leaf Disease Detection")
    uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"])
