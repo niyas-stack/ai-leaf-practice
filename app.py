c5302d35bde5020b55a1e69f1e8c4b0477170c5a
niyas-stack <126703484+niyas-stack@users.noreply.github.com>
2023-05-12 17:58:58 +0530

    Update app.py

--- app.py
+++ app.py
@@ -36,23 +36,31 @@ remedies = {
     ],
     'The above leaf is Cassava CB (Cassava Bacterial Blight)': [
        'Remedy for Cassava Bacterial Blight', 'കാസവ ബാക്ടീരിയൽ ബ്ലൈറ്റിന്റെ പരിഹാരം'
-    ]
+       ]
     # add remedies for other diseases in both English and Malayalam
     
 }
+selected_language = 'English'  # Set the default language
 
-# Preprocessing
-transform = transforms.Compose([
+
+num_ftrs = model.fc.in_features
+model.fc = torch.nn.Linear(num_ftrs, len(classes))
+model_path = "epoch-90.pt"
+model.load_state_dict(torch.load(model_path, map_location=torch.device('cpu')))
+summary(model,input_size=(3,224,224))
+model.eval()
+
+#pre processing
+transform=transforms.Compose([
     transforms.ToTensor(),
-    transforms.Resize((224, 224)),
-    transforms.RandomAffine(degrees=(25)),
-    transforms.RandomRotation(25),
-    transforms.RandomHorizontalFlip(0.5),
-    transforms.RandomVerticalFlip(0.5),
-    transforms.Normalize((0.5, 0.5, 0.5), (1, 1, 1))
+    transforms.Resize((224,224)),
+     transforms.RandomAffine(degrees=(25)),
+     transforms.RandomRotation(25),
+     transforms.RandomHorizontalFlip(0.5),
+     transforms.RandomVerticalFlip(0.5),
+     transforms.Normalize((0.5,0.5,0.5),(1,1,1))
 ])
 
-@st.cache(allow_output_mutation=True)
 def model_predict(image, model_func, transform):
     image_tensor = transform(image).float()
     image_tensor = image_tensor.unsqueeze(0)
@@ -61,46 +69,41 @@ def model_predict(image, model_func, transform):
     pred = classes[index.item()]
     probs, _ = torch.max(F.softmax(output, dim=1), 1)
     if probs < 0.93:
-        return "not defined", probs
+        return "not defined",probs
     else:
         return pred, probs
-
-
 def add_bg_from_local(image_file):
     with open(image_file, "rb") as image_file:
         encoded_string = base64.b64encode(image_file.read())
     st.markdown(
-        f"""
-        <style>
-        .stApp {{
-            background-image: url(data:image/{"jpg"};base64,{encoded_string.decode()});
-            background-size: cover
-        }}
-        </style>
-        """,
-        unsafe_allow_html=True
+    f"""
+    <style>
+    .stApp {{
+        background-image: url(data:image/{"jpg"};base64,{encoded_string.decode()});
+        background-size: cover
+    }}
+    </style>
+    """,
+    unsafe_allow_html=True
     )
 
-
-def get_remedy(pred, selected_language):
+def display_remedies(pred):
     remedy = remedies.get(pred)
     if remedy:
+        st.markdown("<p style= 'color:red;'>Remedy:</p>", unsafe_allow_html=True)
         if selected_language == 'English':
-            return remedy[0]
+            st.info(f" {remedy[0]}")
         else:
-            return remedy[1]
-    else:
-        return "Remedy not available"
-
+            st.info(f" {remedy[1]}")
 
 def main():
+    global selected_language  # Make selected_language global
     st.set_page_config(page_title="AI Leaf Disease Detection", page_icon=":leaves:")
     st.markdown("<h1 style='color: green;'>AI Leaf Disease Detection</h1>", unsafe_allow_html=True)
-    add_bg_from_local('background.jpg')
+    add_bg_from_local('background.jpg')  
 
-    # Check if session state already exists, otherwise initialize it
-    if not hasattr(st.session_state, 'selected_language'):
-        st.session_state.selected_language = 'English'
+    # Language selection
+    selected_language = st.selectbox("Select Language", ['English', 'Malayalam'], index=0)
 
     uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"])
     if uploaded_file is not None:
@@ -111,17 +114,11 @@ def main():
             pred, probs = model_predict(image, model, transform)
             st.markdown(f"<p style='color: red;'>Prediction: {pred}</p>", unsafe_allow_html=True)
             st.markdown(f"<p style='color: red;'>Probability: {probs.item()}</p>", unsafe_allow_html=True)
-
-            # Language selection for remedy
-            selected_language = st.selectbox("Select Remedy Language", ['English', 'Malayalam'], index=0)
-            st.session_state.selected_language = selected_language
-
-            remedy = get_remedy(pred, selected_language)
-            st.markdown("<p style= 'color:red;'>Remedy:</p>", unsafe_allow_html=True)
-            st.info(f" {remedy}")
+            display_remedies(pred)
 
 
-if __name__ == "__main__":
-    main()
 
 
+
+if _name_ == "_main_":
+    main()
