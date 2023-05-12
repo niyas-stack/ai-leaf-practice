4295f0b05322931c82535970216c1b6571b8257c
niyas-stack <126703484+niyas-stack@users.noreply.github.com>
2023-05-12 19:32:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -123,11 +123,11 @@ def main():
 
     if classify_button_clicked:
         selected_language = st.selectbox("Select Language", ['English', 'Malayalam'], index=0, key="language_select")
-
-    if selected_language == 'Malayalam':
-        display_remedies_malayalam(pred)
-    else:
-        display_remedies(pred)
+        if selected_language == 'Malayalam':
+            display_remedies_malayalam(pred)
+        else:
+            display_remedies(pred)
 
 if __name__ == "__main__":
     main()
+
