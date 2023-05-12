a5faf9438bc97f2606830d2aa783218ef15aa2ed
niyas-stack <126703484+niyas-stack@users.noreply.github.com>
2023-05-12 18:09:43 +0530

    Update app.py

--- app.py
+++ app.py
@@ -101,10 +101,6 @@ def main():
     st.set_page_config(page_title="AI Leaf Disease Detection", page_icon=":leaves:")
     st.markdown("<h1 style='color: green;'>AI Leaf Disease Detection</h1>", unsafe_allow_html=True)
     add_bg_from_local('background.jpg')  
-
-    # Language selection
-    selected_language = st.selectbox("Select Language", ['English', 'Malayalam'], index=0)
-
     uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"])
     if uploaded_file is not None:
         image = Image.open(uploaded_file)
@@ -114,9 +110,10 @@ def main():
             pred, probs = model_predict(image, model, transform)
             st.markdown(f"<p style='color: red;'>Prediction: {pred}</p>", unsafe_allow_html=True)
             st.markdown(f"<p style='color: red;'>Probability: {probs.item()}</p>", unsafe_allow_html=True)
-            display_remedies(pred)
-
-
+            
+       # Language selection
+    selected_language = st.selectbox("Select Language", ['English', 'Malayalam'], index=0)
+    display_remedies(pred)
 
 
 
