86616b95ee9269c210d4a4a1a78fc8c8689ac0b3
niyas-stack <126703484+niyas-stack@users.noreply.github.com>
2023-05-19 04:36:35 +0530

    Update app.py

--- app.py
+++ app.py
@@ -140,6 +140,15 @@ def main():
     st.set_page_config(page_title="AI Leaf Disease Detection", page_icon=":leaves:")  
     st.markdown('<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">', unsafe_allow_html=True)
     st.markdown("""
+    <style>
+    .navbar-brand img {
+        width: 30px;
+        height: 24px;
+        /* Other styling properties */
+    }
+    </style>
+    """, unsafe_allow_html=True)
+    st.markdown("""
     <nav class="navbar bg-body-tertiary">
   <div class="container-fluid">
     <a class="navbar-brand" href="#">
