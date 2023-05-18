9c2a74bfc169ad041d5ef791009f1bd6c794a41d
niyas-stack <126703484+niyas-stack@users.noreply.github.com>
2023-05-19 04:13:06 +0530

    Update app.py

--- app.py
+++ app.py
@@ -204,6 +204,6 @@ def main():
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
     """, unsafe_allow_html=True)
             
-if _name_ == "_main_":
+if __name__ == "__main__":
    main()
 
