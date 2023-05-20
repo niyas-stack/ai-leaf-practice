5cc107be29551b489651fadb60492e828f5494ac
niyas-stack <126703484+niyas-stack@users.noreply.github.com>
2023-05-20 23:16:51 +0530

    Update app.py

--- app.py
+++ app.py
@@ -12,6 +12,35 @@ st.markdown("""
     }
 </style>
 """,unsafe_allow_html=True)
+st.markdown("""
+<style>
+    #MainMenu, header, footer {visibility: hidden;}
+
+    /* This code gets the first element on the sidebar,
+    and overrides its default styling */
+    section[data-testid="stSidebar"] div:first-child {
+        top: 0;
+        height: 100vh;
+    }
+</style>
+""",unsafe_allow_html=True)
+# Render the fixed header
+st.markdown(
+    f"""
+    <header style="{header_style}">
+        <h1>Project Title</h1>
+        <img src="logo.png" alt="Logo" style="{logo_style}">
+        <nav>
+            <a href="#">Home</a>
+            <a href="#">About</a>
+            <a href="#">Contact</a>
+        </nav>
+    </header>
+    """,
+    unsafe_allow_html=True
+)
+
+
 
 st.title('This is a test app')
 
