4dd80495df933160087f7ae61180e47ef040e921
niyas-stack <126703484+niyas-stack@users.noreply.github.com>
2023-05-20 22:50:17 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,56 +1,14 @@
 import streamlit as st
 
 # Custom CSS styles
-custom_css = """
-<style>
-body {
-    margin: 0;
-    padding: 0;
-}
-
-.stApp {
-    background-color: transparent;
-}
-
-.stButton>button {
-    background-color: transparent;
-    color: inherit;
-}
-
-.stButton>button:hover {
-    background-color: transparent;
-}
-
-.stTextInput>div>div {
-    background-color: transparent;
-}
-
-.stTextInput>div>input {
-    color: inherit;
-}
-
-.stMarkdown {
-    background-color: transparent;
-    color: inherit;
-}
-
-.stMarkdown p {
-    margin: 0;
-    padding: 0;
-}
-
-.stMarkdown a {
-    color: inherit;
-}
-
-.stMarkdown a:hover {
-    text-decoration: underline;
-}
-</style>
+hide_header_style = """
+    <style>
+    #MainMenu {visibility: hidden;}
+    </style>
 """
 
-# Render the custom CSS
-st.markdown(custom_css, unsafe_allow_html=True)
+# Render the custom CSS to hide the default Streamlit header
+st.markdown(hide_header_style, unsafe_allow_html=True)
 
 # Render the fixed header
 st.markdown(
@@ -71,7 +29,5 @@ st.markdown(
 # Rest of your Streamlit app code goes here
 st.title("Welcome to My Streamlit App")
 st.write("This is the content of your app.")
-
-
 
 
