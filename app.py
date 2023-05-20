91f150f4413e3ab9f45b00f648aac06feaf7d639
niyas-stack <126703484+niyas-stack@users.noreply.github.com>
2023-05-21 04:04:47 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,33 +8,19 @@ st.markdown('''
 .stApp [data-testid="stToolbar"]{
     display:none;
 }
-.header-container {
-    display: flex;
-    align-items: center;
-}
-.header-container img {
-    width: 100px;
-    margin-right: 20px;
-}
-
-@media (max-width: 600px) {
-  .header-container {
-    flex-direction: row;
-    align-items: center;
-  }
-  .header-container img {
-    margin-right: 10px;
-  }
-}
 </style>
 ''', unsafe_allow_html=True)
 
 # Create the header section
-header_container = st.container()
+header_container = st.beta_container()
 
 # Add logo and title to the header
-with header_container:
+header_columns = header_container.beta_columns([2, 6])  # Adjust column widths as needed
+
+with header_columns[0]:
     st.image('logo.png', width=100)
+
+with header_columns[1]:
     st.title('My Website')
 
 # Navigation options
