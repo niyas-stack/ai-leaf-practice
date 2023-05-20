b89f97462b78317e73d22cc1c8695715d43dd534
niyas-stack <126703484+niyas-stack@users.noreply.github.com>
2023-05-21 03:47:14 +0530

    Update app.py

--- app.py
+++ app.py
@@ -26,23 +26,11 @@ header_container = st.beta_container()
 header_columns = header_container.beta_columns([1, 6])  # Adjust column widths as needed
 
 with header_columns[0]:
-    st.markdown('<div class="logo"><img src="logo.png" alt="Logo"></div>', unsafe_allow_html=True)
+    st.image('logo.png', use_column_width=False, caption='Logo', width=100, class_='logo')
 
 with header_columns[1]:
     st.title('My Website')
 
-# Navigation options
-nav_option = st.sidebar.radio('Go to', ('Home', 'About', 'Contact'))
-
-# Render different content based on the selected navigation option
-if nav_option == 'Home':
-    st.title('Home Page')
-    # Add content for the home page
-elif nav_option == 'About':
-    st.title('About Page')
-    # Add content for the about page
-elif nav_option == 'Contact':
-    st.title('Contact Page')
-    # Add content for the contact page
+# Navigation options and content rendering code...
 
 
