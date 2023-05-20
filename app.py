bf3c7d317e9992a0df71348368818bfecbf419ae
niyas-stack <126703484+niyas-stack@users.noreply.github.com>
2023-05-20 23:37:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ logo_style = """
 # Hide Streamlit default styles
 hide_menu_style = """
     <style>
-    #Header { visibility: hidden; }
+    #Header{ visibility: hidden; }
     </style>
 """
 st.markdown(hide_menu_style, unsafe_allow_html=True)
