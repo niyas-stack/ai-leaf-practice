b7e180bdb0b7416d5ced33715246242a0e309523
niyas-stack <126703484+niyas-stack@users.noreply.github.com>
2023-05-21 00:35:58 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,54 @@
 import streamlit as st
 
-enable_scroll = """
-<style>
-.main {
-    overflow: auto;
-}
-</style>
+# Custom CSS styles
+header_style = """
+    position: fixed;
+    top: 0;
+    left: 0;
+    right: 0;
+    height: 60px;
+    background-color: yellow;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    padding: 0 20px;
+    z-index: 999;
 """
 
-st.markdown(enable_scroll, unsafe_allow_html=True)
+logo_style = """
+    height: 40px;
+    width: auto;
+"""
 
-st.title("Test")
-if st.checkbox('Remove padding'):
-    st.markdown(hide_streamlit_style, unsafe_allow_html=True)
+# Render custom CSS styles
+st.markdown(
+    f"""
+    <style>
+    /* Hide Streamlit header */
+    .stAppHeader {{
+        display: none;
+    }}
+    </style>
+    """,
+    unsafe_allow_html=True
+)
+
+# Render the fixed header
+st.markdown(
+    f"""
+    <header style="{header_style}">
+        <h1>Project Title</h1>
+        <img src="logo.png" alt="Logo" style="{logo_style}">
+        <nav>
+            <a href="#">Home</a>
+            <a href="#">About</a>
+            <a href="#">Contact</a>
+        </nav>
+    </header>
+    """,
+    unsafe_allow_html=True
+)
+
+# Rest of your Streamlit app code goes here
+st.title("Welcome to My Streamlit App")
+st.write("This is the content of your app.")
