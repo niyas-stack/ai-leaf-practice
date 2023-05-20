d8632113ad06a40252a068183b2f7017231ce59e
niyas-stack <126703484+niyas-stack@users.noreply.github.com>
2023-05-20 21:59:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,12 +3,17 @@ import streamlit as st
 # Set page title and favicon
 st.set_page_config(page_title='My Website', page_icon='logo.png')
 
-# Create the navigation bar
-st.sidebar.title('Navigation')
+# Create the header section
+header_container = st.beta_container()
 
-# Add logo and title to the navigation bar
-st.sidebar.image('logo.png', use_column_width=True)
-st.sidebar.title('My Website')
+# Add logo and title to the header
+header_columns = header_container.beta_columns([1, 6])  # Adjust column widths as needed
+
+with header_columns[0]:
+    st.image('logo.png', use_column_width=True)
+
+with header_columns[1]:
+    st.title('My Website')
 
 # Navigation options
 nav_option = st.sidebar.radio('Go to', ('Home', 'About', 'Contact'))
