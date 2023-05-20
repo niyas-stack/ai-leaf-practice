a86d400baa7269a5ee3c3c0402710f4f490d968a
niyas-stack <126703484+niyas-stack@users.noreply.github.com>
2023-05-21 03:14:35 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,38 +1,7 @@
 import streamlit as st
-import validators
-import base64
-from pathlib import Path
-def add_logo(logo_url: str, height: int = 120):
-    """Add a logo (from logo_url) on the top of the navigation page of a multipage app.
-    Taken from https://discuss.streamlit.io/t/put-logo-and-title-above-on-top-of-page-navigation-in-sidebar-of-multipage-app/28213/6
 
-    The url can either be a url to the image, or a local path to the image.
-
-    Args:
-        logo_url (str): URL/local path of the logo
-    """
-
-    if validators.url(logo_url) is True:
-        logo = f"url({logo_url})"
-    else:
-        logo = f"url(data:image/png;base64,{base64.b64encode(Path(logo_url).read_bytes()).decode()})"
-
-    st.markdown(
-        f"""
-        <style>
-            [data-testid="stSidebarNav"] {{
-                background-image: {logo};
-                background-repeat: no-repeat;
-                padding-top: {height - 40}px;
-                background-position: 20px 20px;
-            }}
-        </style>
-        """,
-        unsafe_allow_html=True,
-    )
 # Set page title and favicon
 st.set_page_config(page_title='My Website', page_icon='logo.png')
-add_logo("logo.png", height=120)
 
 # Create the header section
 header_container = st.beta_container()
@@ -45,6 +14,21 @@ with header_columns[0]:
 
 with header_columns[1]:
     st.title('My Website')
+
+# Add custom CSS for the navigation bar
+st.markdown(
+    """
+    <style>
+    .sidebar .sidebar-content {
+        background-image: url('logo.png');
+        background-repeat: no-repeat;
+        background-position: 20px 20px;
+        padding-top: 120px; /* Adjust the padding as needed */
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
 
 # Navigation options
 nav_option = st.sidebar.radio('Go to', ('Home', 'About', 'Contact'))
@@ -59,3 +43,4 @@ elif nav_option == 'About':
 elif nav_option == 'Contact':
     st.title('Contact Page')
     # Add content for the contact page
+
