1a925d72c4af10b501248f84b4b709dac77afb25
niyas-stack <126703484+niyas-stack@users.noreply.github.com>
2023-05-21 00:45:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,34 @@
 import streamlit as st
 
+# Set page title and favicon
+st.set_page_config(page_title='My Website', page_icon='logo.png')
+
+# Create the header section
+header_container = st.beta_container()
+
+# Add logo and title to the header
+header_columns = header_container.beta_columns([1, 6])  # Adjust column widths as needed
+
+with header_columns[0]:
+    st.image('logo.png', use_column_width=True)
+
+with header_columns[1]:
+    st.title('My Website')
+
+# Navigation options
+nav_option = st.sidebar.radio('Go to', ('Home', 'About', 'Contact'))
+
+# Render different content based on the selected navigation option
+if nav_option == 'Home':
+    st.title('Home Page')
+    # Add content for the home page
+elif nav_option == 'About':
+    st.title('About Page')
+    # Add content for the about page
+elif nav_option == 'Contact':
+    st.title('Contact Page')
+    # Add content for the contact page
+
 # Custom CSS styles
 header_style = """
     position: fixed;
@@ -51,4 +80,5 @@ st.markdown(
 
 # Rest of your Streamlit app code goes here
 st.title("Welcome to My Streamlit App")
-st.write("This is the content of your app.")
+st.write("This is the content of your app.")
+
