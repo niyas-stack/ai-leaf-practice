8303a25000428ccec830b3c261bcf2cae8545c40
niyas-stack <126703484+niyas-stack@users.noreply.github.com>
2023-05-22 02:30:11 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,9 @@
 import streamlit as st
 
-# Set the CSS style for the text area
-text_area_style = """
-    background-color: black;
-    color: white;
-"""
+# Predetermined text
+predetermined_text = "This is predetermined text."
 
-# Add the text area with the modified style
-text_input = st.text_area("Enter your text", value="", height=200)
+# Display the text area with predetermined text
+text_input = st.text_area("Enter your text", value=predetermined_text, height=200)
 
 # You can continue with the rest of your Streamlit app code
